402500f0843c479dff6a7b473f8c5b3d22ca1e55
Gourav Mahor <gourav.mahor@bpaassolutions.com>
2025-08-04 14:52:14 +0530

    first commit

---
+++ mdmservice/src/main/java/com/tech/MdmserviceApplication.java
@@ -0,0 +1,13 @@
+package com.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdmserviceApplication.class, args);
+	}
+
+}
