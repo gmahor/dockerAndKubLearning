ef112d8e2aa19680e56d6f3ad9b8b923c0e27bdd
vishwajeet.deshmukh <vishwajeet.deshmukh@bpaassolutions.com>
2025-08-04 17:15:25 +0530

    common mdm classed added to auth service

---
+++ userauth/src/main/java/com/tech/enities/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.tech.enities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
--- userauth/src/main/java/com/tech/repos/UserRepo.java
+++ userauth/src/main/java/com/tech/repos/UserRepo.java
@@ -1,4 +1,12 @@
 package com.tech.repos;
 
-public interface UserRepo {
+import com.tech.enities.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users,Long> {
+    Optional<Users> findByUsername(String username);
 }
